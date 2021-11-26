193605906aa96874c72ca4fc093004d7f07c4e31
Jethro Williams <jethrowilliams@outlook.com>
2021-11-26 17:37:40 +0000

    Add the color 'brand' and use it in the jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ export default function App() {
             <h1 className='mt-6 text-2xl font-bold text-gray-900 sm:mt-8 sm:text-4xl lg:text-3xl xl:text-4xl'>
               You can work from anywhere.
               <br className='hidden lg:inline' />{' '}
-              <span className='text-indigo-500'>Take advantage of it.</span>
+              <span className='text-brand'>Take advantage of it.</span>
             </h1>
             <p className='mt-2 text-gray-600 sm:mt-4 sm:text-xl'>
               Workcation helps you find work-friendly rentals in beautiful locations so you can
@@ -26,7 +26,7 @@ export default function App() {
             </p>
             <div className='mt-4 space-x-1 sm:mt-6'>
               <a
-                className='inline-block px-5 py-3 rounded-lg transform transition bg-indigo-500 hover:bg-indigo-400 hover:-translate-y-0.5 focus:ring-indigo-500 focus:ring-opacity-50 focus:outline-none focus:ring focus:ring-offset-2 active:bg-indigo-600 uppercase tracking-wider font-semibold text-sm text-white shadow-lg sm:text-base'
+                className='inline-block px-5 py-3 rounded-lg transform transition bg-brand hover:bg-indigo-400 hover:-translate-y-0.5 focus:ring-brand focus:ring-opacity-50 focus:outline-none focus:ring focus:ring-offset-2 active:bg-indigo-600 uppercase tracking-wider font-semibold text-sm text-white shadow-lg sm:text-base'
                 href='#'
               >
                 Book your escape
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,18 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: '#0fa9e6',
+      },
+    },
   },
   variants: {
     extend: {
-      backgroundColor: ["active"],
+      backgroundColor: ['active'],
     },
   },
   plugins: [],
 };
+
+// By default, 'theme' will inherit the default tailwind theme
